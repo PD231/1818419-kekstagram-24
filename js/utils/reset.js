30a3f838a18bc8595928e71130df9886f6818651
PD231 <paveldontsov@mail.ru>
2021-11-02 16:55:36 +0300

    10.6. Помощь друга

--- js/utils/reset.js
+++ js/utils/reset.js
@@ -1,13 +1,28 @@
 
+import { preview, destroySlider} from '../upload-photo.js';
 const closeButtom = document.querySelector('.img-upload__cancel');
 const uploadForm = document.querySelector('.img-upload__form');
-const preview = uploadForm.querySelector('.img-upload__preview');
+const controlValue = uploadForm.querySelector('.scale__control--value');
 
 const reset = () => {
   closeButtom.addEventListener('click', () => {
     preview.style = '';
+    controlValue.value = 0;
     preview.querySelector('img').classList.value = '';
     uploadForm.reset();
+    destroySlider();
+    preview.querySelector('img').style = '';
+  });
+
+  document.addEventListener('keydown', (evt) => {
+    if ((evt.key === 'Escape') && (!evt.target.closest('.text__description, .text__hashtags'))) {
+      preview.style = '';
+      controlValue.value = 0;
+      preview.querySelector('img').classList.value = '';
+      uploadForm.reset();
+      destroySlider();
+      preview.querySelector('img').style = '';
+    }
   });
 };
 
